6f93e29355f42bc03ced56ca696cf47abb21e7d5
eapinis <evan.apinis@gmail.com>
2024-12-20 12:50:54 -0500

    Started verification of core, completed ADD and SUB

--- Single-Cycle/sim/soc_top/dofile.do
+++ Single-Cycle/sim/soc_top/dofile.do
@@ -6,6 +6,8 @@ add wave -position end sim:/soc_top_tb/DUT/*
 add wave -position end sim:/soc_top_tb/DUT/core_1/*
 add wave -position end sim:/soc_top_tb/DUT/core_1/register_file/*
 add wave -position end sim:/soc_top_tb/DUT/core_1/register_file/reg_array
+add wave -position end sim:/soc_top_tb/DUT/instruction_mem/*
+add wave -position end sim:/soc_top_tb/DUT/instruction_mem/instr_mem
 
 run -all
 
--- Single-Cycle/sim/soc_top/soc_top_tb.v
+++ Single-Cycle/sim/soc_top/soc_top_tb.v
@@ -15,6 +15,11 @@ parameter CLK_PERIOD = 10;
 
 reg Clk_Core_IF, Clk_Core_MEM, Clk_Core_WB, Rst_Core_N;
 
+integer file, line_num, i, target_pc, Test_Failed;
+reg [31:0] instr;
+reg [31:0] init;
+reg [31:0] expected;
+
 ////////////////////////////////////////////////////////////////
 //////////////////////   Instantiations   //////////////////////
 ////////////////////////////////////////////////////////////////
@@ -61,21 +66,191 @@ initial begin
       @(posedge Clk_Core_IF);
     end
     Rst_Core_N = 1;
-    repeat (10) begin
-      @(posedge Clk_Core_IF);
-    end
-    $finish;
 end
 
+// Run Tests
 initial begin
-    DUT.instruction_mem.instr_mem[0] = 32'h3e800093;
-    DUT.instruction_mem.instr_mem[1] = 32'h7d008113;
-    DUT.instruction_mem.instr_mem[2] = 32'hc1810193;
-    DUT.instruction_mem.instr_mem[3] = 32'h83018213;
-    DUT.instruction_mem.instr_mem[4] = 32'h3e820293;
-    DUT.instruction_mem.instr_mem[5] = 32'h00010317;
-    DUT.instruction_mem.instr_mem[6] = 32'hfec30313;
-    DUT.instruction_mem.instr_mem[7] = 32'h00430313;
+
+    Test_Failed = 0;
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                              ADD                                             //
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Begin testing add instructions
+    file = $fopen("../../programs/add.hex", "r");
+    if (file == 0) begin
+        $display("ERROR: Failed to open file ../../programs/add.hex");
+        $finish;
+    end
+    
+    // Load instructions into mem
+    line_num = 0;
+    while (!$feof(file)) begin
+        if ($fscanf(file, "%h\n", instr) > 0) begin
+            force DUT.instruction_mem.instr_mem[line_num] = instr;
+            line_num = line_num + 1;
+        end
+    end
+    $fclose(file);
+    //$display("Instruction memory initialized from ../../programs/add.hex");
+    
+    target_pc = line_num * 4;
+    
+    file = $fopen("../../programs/add_reg_init.hex", "r");
+    if (file == 0) begin
+        $display("ERROR: Failed to open file ../../programs/add_reg_init.hex");
+        $finish;
+    end
+    
+    i = 0;
+    while (!$feof(file)) begin
+        if ($fscanf(file, "%h\n", init) > 0) begin
+            DUT.core_1.register_file.reg_array[i] = init;
+            i = i + 1;
+        end
+    end
+    $fclose(file);
+    //$display("Register file initialized from ../../programs/add_reg_init.hex");
+    
+    $display("\n################################################################################\n");
+    
+    $display("RUNNING TESTS FOR ADD INSTRUCTION\n");
+
+    wait (target_pc == DUT.core_1.program_counter.program_count_reg);
+
+    for (i = 0; i < line_num; i = i + 1) begin
+        release DUT.instruction_mem.instr_mem[i];
+    end
+    
+    file = $fopen("../../programs/add_reg_final.hex", "r");
+    if (file == 0) begin
+        $display("ERROR: Failed to open file ../../programs/add_reg_final.hex");
+        $finish;
+    end
+    
+    i = 0;
+    while (!$feof(file)) begin
+        if ($fscanf(file, "%h\n", expected) > 0) begin
+            $display("Register Address: %h Expected Value: %h Calculated Value: %h", i, expected, DUT.core_1.register_file.reg_array[i]);
+            if (DUT.core_1.register_file.reg_array[i] != expected) begin
+                Test_Failed = 1;
+            end
+            i = i + 1;
+        end
+    end
+    
+    if (Test_Failed == 1) begin
+        $display("\nADD INSTRUCTION TESTING FAILED");
+    end
+    else begin
+        $display("\nADD INSTRUCTION TESTING PASSED");
+    end
+    
+    Test_Failed = 0;
+    
+    $display("\n################################################################################\n");
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    Rst_Core_N = 0;
+    repeat (2) begin
+      @(posedge Clk_Core_IF);
+    end
+    Rst_Core_N = 1;
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                              SUB                                             //
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Begin testing sub instructions
+    file = $fopen("../../programs/sub.hex", "r");
+    if (file == 0) begin
+        $display("ERROR: Failed to open file ../../programs/sub.hex");
+        $finish;
+    end
+    
+    // Load instructions into mem
+    line_num = 0;
+    while (!$feof(file)) begin
+        if ($fscanf(file, "%h\n", instr) > 0) begin
+            force DUT.instruction_mem.instr_mem[line_num] = instr;
+            line_num = line_num + 1;
+        end
+    end
+    $fclose(file);
+    //$display("Instruction memory initialized from ../../programs/sub.hex");
+    
+    target_pc = line_num * 4;
+    
+    file = $fopen("../../programs/sub_reg_init.hex", "r");
+    if (file == 0) begin
+        $display("ERROR: Failed to open file ../../programs/sub_reg_init.hex");
+        $finish;
+    end
+    
+    i = 0;
+    while (!$feof(file)) begin
+        if ($fscanf(file, "%h\n", init) > 0) begin
+            DUT.core_1.register_file.reg_array[i] = init;
+            i = i + 1;
+        end
+    end
+    $fclose(file);
+    //$display("Register file initialized from ../../programs/sub_reg_init.hex");
+    
+    $display("\n################################################################################\n");
+    
+    $display("RUNNING TESTS FOR SUB INSTRUCTION\n");
+
+    wait (target_pc == DUT.core_1.program_counter.program_count_reg);
+
+    for (i = 0; i < line_num; i = i + 1) begin
+        release DUT.instruction_mem.instr_mem[i];
+    end
+    
+    file = $fopen("../../programs/sub_reg_final.hex", "r");
+    if (file == 0) begin
+        $display("ERROR: Failed to open file ../../programs/sub_reg_final.hex");
+        $finish;
+    end
+    
+    i = 0;
+    while (!$feof(file)) begin
+        if ($fscanf(file, "%h\n", expected) > 0) begin
+            $display("Register Address: %h Expected Value: %h Calculated Value: %h", i, expected, DUT.core_1.register_file.reg_array[i]);
+            if (DUT.core_1.register_file.reg_array[i] != expected) begin
+                Test_Failed = 1;
+            end
+            i = i + 1;
+        end
+    end
+    
+    if (Test_Failed == 1) begin
+        $display("\nSUB INSTRUCTION TESTING FAILED");
+    end
+    else begin
+        $display("\nSUB INSTRUCTION TESTING PASSED");
+    end
+
+    Test_Failed = 0;
+    
+    $display("\n################################################################################\n");
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    Rst_Core_N = 0;
+    repeat (2) begin
+      @(posedge Clk_Core_IF);
+    end
+    Rst_Core_N = 1;
+
+    $finish;
+    
 end
 
 endmodule
